f1ec4886bb7a249949aa102257f94a557a15a7a5
root14 <ilkaygavaz2@gmail.com>
2020-09-29 14:10:05 +0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minne/styles.dart';
 
 class loginPage extends StatefulWidget {
   @override
@@ -9,8 +10,40 @@ class _loginPageState extends State<loginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('yo yo yo'),),
-      body: Text('yo bro its just a text relaxa'),
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.1, 0.4, 0.7, 0.9],
+            colors: [
+              Color(0xFF3594DD),
+              Color(0xFF4563DB),
+              Color(0xFF5036D5),
+              Color(0xFF5B16D0),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Login'),
+              Image(
+                image: AssetImage('assets/assets1.jpg'),
+                height: 450,
+                width: 250,
+              ),
+              TextField(
+                autofocus: true,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'password'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
