a0c494fdd02a242f4b8bebcff7ffabcb230f4560
root14 <ilkaygavaz2@gmail.com>
2020-10-07 22:29:11 +0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -6,13 +6,15 @@ class login extends StatefulWidget {
   @override
   _loginState createState() => _loginState();
 }
+//KATILDIĞIM EVENTLER TODO:
+//DRAWER
 
 class _loginState extends State<login> {
   final List<userModel> users = [
     userModel("1", "lorem", "abc@mail.com", "https://picsum.photos/250?image=9",
         123, 321),
-        userModel("2", "ipsum", "def@mail.com", "https://picsum.photos/250",
-        123, 321),
+    userModel(
+        "2", "ipsum", "def@mail.com", "https://picsum.photos/250", 123, 321),
   ];
 
   Widget buildUsersCard(BuildContext context, int index) {
@@ -94,7 +96,52 @@ class _loginState extends State<login> {
               )),
         ),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://static.wikia.nocookie.net/ortadunya/images/1/12/Gandalf2.jpg/revision/latest?cb=20191223114037'),
+                          radius: 43,
+                    
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        'YOUR_NAME_HERE',
+                        style: Styles.innerTitle
+                      ),
+                    ),
+                  ],
+                )),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text(
+                'Mesajlar',
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                'Profilim',
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Ayarlar',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
